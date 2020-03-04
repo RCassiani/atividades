5471da874e6b5c0b930d13deab5db5ab06eb5174
Rodrigo Cassiani <rodcassiani@gmail.com>
2020-03-03 22:28:02 -0300

    Atividades 1,2,3

---
+++ atv1/index.php
@@ -0,0 +1,39 @@
+<?php
+    //Array desordenado
+    $location = [
+        ['pais' => 'Estados Unidos', 'capital' => 'Washington'],
+        ['pais' => 'Brasil', 'capital' => 'Brasília'],
+        ['pais' => 'Rússia', 'capital' => 'Moscou'],
+        ['pais' => 'Argentina', 'capital' => 'Buenos Aires'],
+        ['pais' => 'Canadá', 'capital' => 'Ottawa'],
+    ];
+
+    //Função de comparação para ordenar
+    function order($a, $b)
+    {
+        return $a['capital'] > $b['capital'];
+    }
+
+    // Array ordenado
+    usort($location, 'order');
+?>
+
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Atividade 1</title>
+</head>
+<body>
+    <ul>
+        <?php
+        //Listando arrays
+        foreach ($location as $l)
+            echo "<li>" . $l['capital'] . " é a capital do(a) " . $l['pais'] . "</li>";
+        ?>
+    </ul>
+</body>
+</html>
--- atv2/index.php
+++ atv2/index.php
@@ -1,20 +1,13 @@
 <?php
-    //Array desordenado
-    $location = [
-        ['pais' => 'Estados Unidos', 'capital' => 'Washington'],
-        ['pais' => 'Brasil', 'capital' => 'Brasília'],
-        ['pais' => 'Rússia', 'capital' => 'Moscou'],
-        ['pais' => 'Argentina', 'capital' => 'Buenos Aires'],
-        ['pais' => 'Canadá', 'capital' => 'Ottawa'],
-    ];
 
-    //Função de comparação
-    function order($a, $b) {
-        return $a['capital'] > $b['capital'];
+    //Função foiMordido
+    function foiMordido()
+    {
+        return rand(0, 1);
     }
 
-    // Array ordenado
-    usort($location, 'order');
+    //Declaração de mordeu
+    $mordeu = (isset($_GET['mordeu']) == 1) ? foiMordido() : 0;
 ?>
 
 <!doctype html>
@@ -24,13 +17,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Atividade 1</title>
+    <title>Atividade 2</title>
 </head>
 <body>
-    <?php
-    //Listando arrays
-    foreach($location as $l)
-        echo "<li>".$l['capital']." é a capital do(a) ".$l['pais'];
-    ?>
+    <a href="index.php?mordeu=1">
+        <button type="button">Joãozinho foi mordido?</button>
+    </a>
+    <p>Joaozinho <?= (!$mordeu) ? 'NAO' : '' ?> mordeu seu dedo</p>
 </body>
 </html>
